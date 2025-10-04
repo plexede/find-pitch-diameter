3bf14785fd6b2c2b5ea623815bf3bebfc8003a7f
okm <64970074+plexede@users.noreply.github.com>
2025-10-04 11:09:38 -0400

    wip 3 compound gear calc

--- pitch_diameter.py
+++ pitch_diameter.py
@@ -2,29 +2,36 @@
 # The diameter of a gear from its center to its ideal meshing point can be represented as the Pitch Diameter.
 # Pitch Diameter = Module * Number of Teeth
 # Using this, the ideal distance between two gears can be calculated by finding the sum of both gears’ pitch diameters and dividing them by two.
-module = 1
-# var centerDistance # mm distance between two axles
 
-numberOfTeeth1 = 80
-numberOfTeeth2 = 40
-pitchDiameter1 = module * numberOfTeeth1
-pitchDiameter2 = module * numberOfTeeth2
+# The goal is to input a ratio, and output the number of gear teeth between 3 compound gears.
 
+MODULE = 1
+CENTER_DISTANCE = 60 # mm
 # centerDistance = (pitchDiameter1 + pitchDiameter2) / 2
 
-centerDistance = 60 # mm
-idealPitchDiameter = (2 * centerDistance ) - pitchDiameter1
-idealPitchDiameter /= 2
+pitchDiameter1 = MODULE * inputNumberOfTeeth
 
-numberOfTeeth1 = pitchDiameter1 / module
-print("number of teeth on gear 1:", numberOfTeeth1)
+def findIdealGearSize(centerDistance, pitchDiameter1):
+    idealPitchDiameter = (2 * centerDistance ) - pitchDiameter1
+    idealPitchDiameter /= 2
+    return idealPitchDiameter
+
+inputNumberOfTeeth = pitchDiameter1 / MODULE
+print("number of teeth on gear 1:", inputNumberOfTeeth)
 
 # print(pitchDiameter2)
 
-numberOfTeeth2 = idealPitchDiameter / module
+numberOfTeeth2 = idealPitchDiameter / MODULE
 print("ideal number of teeth on gear 2:", numberOfTeeth2)
 
-print("the ratio of gear 1 to gear 2 is:", int(numberOfTeeth1 / numberOfTeeth2), ": 1")
+int(ratio) = inputNumberOfTeeth / numberOfTeeth2
+print("the ratio of gear 1 to gear 2 is:", ratio, ": 1")
+
+
+# find specific ratio
+targetRatio = 2.5
+while ratio != targetRatio
+    inputNumberOfTeeth
 
 
 # print(centerDistance)
