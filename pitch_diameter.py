8536f85e93c6d7ceb5349dd473f7801c786a68db
plex <64970074+plexede@users.noreply.github.com>
2025-10-04 16:10:56 -0400

    input ratio, output 2 gears need to add second set of compound gears

--- pitch_diameter.py
+++ pitch_diameter.py
@@ -5,33 +5,32 @@
 
 # The goal is to input a ratio, and output the number of gear teeth between 3 compound gears.
 
+import random
+
 MODULE = 1
 CENTER_DISTANCE = 60 # mm
+TARGET_RATIO = 1.5 # ratio to be solved for
 # centerDistance = (pitchDiameter1 + pitchDiameter2) / 2
+ratioFound = False
+inputNumberOfTeeth = 1
+while not ratioFound:
+    pitchDiameter1 = MODULE * inputNumberOfTeeth
 
-pitchDiameter1 = MODULE * inputNumberOfTeeth
+    def findIdealGearSize(centerDistance, pitchDiameter1):
+        idealPitchDiameter = (2 * centerDistance ) - pitchDiameter1
+        idealPitchDiameter /= 2
+        return idealPitchDiameter
 
-def findIdealGearSize(centerDistance, pitchDiameter1):
-    idealPitchDiameter = (2 * centerDistance ) - pitchDiameter1
-    idealPitchDiameter /= 2
-    return idealPitchDiameter
-
-inputNumberOfTeeth = pitchDiameter1 / MODULE
-print("number of teeth on gear 1:", inputNumberOfTeeth)
-
-# print(pitchDiameter2)
-
-numberOfTeeth2 = idealPitchDiameter / MODULE
-print("ideal number of teeth on gear 2:", numberOfTeeth2)
-
-int(ratio) = inputNumberOfTeeth / numberOfTeeth2
-print("the ratio of gear 1 to gear 2 is:", ratio, ": 1")
-
-
-# find specific ratio
-targetRatio = 2.5
-while ratio != targetRatio
-    inputNumberOfTeeth
-
-
-# print(centerDistance)
+    # if not a whole number, discard
+    numberOfTeeth2 = findIdealGearSize(CENTER_DISTANCE, pitchDiameter1) / MODULE
+    ratio = inputNumberOfTeeth / numberOfTeeth2
+    if (numberOfTeeth2 % 1) == 0: # if it is a whole number
+        if ratio >= TARGET_RATIO:
+            ratioFound = True
+        else:
+            print(f"Tried {inputNumberOfTeeth} teeth on gear 1, gives {int(numberOfTeeth2)} teeth on gear 2 with a ratio of {ratio}:1")
+    inputNumberOfTeeth += 1 # increment and try again
+pitchDiameter2 = MODULE * numberOfTeeth2
+print(f"Gear 1: {inputNumberOfTeeth} teeth, Pitch Diameter: {pitchDiameter1} mm")
+print(f"Gear 2: {int(numberOfTeeth2)} teeth, Pitch Diameter: {pitchDiameter2} mm")
+print(f"Ratio: {ratio}:1")
